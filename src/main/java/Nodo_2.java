1e640bbe6b0d56c97bf78712609f7c81ecdf0b80
pradeus <andresps1408@gmail.com>
2020-11-07 23:03:05 -0600

    clase nodo 2 modificada y lista circular

--- src/main/java/Nodo_2.java
+++ src/main/java/Nodo_2.java
@@ -1,8 +1,14 @@
 public class Nodo_2 {
-    Carta carta_en_mano;
+    public Carta carta_en_mano;
     Nodo_2 next;
-
-    public Nodo_2(Carta carta_en_mano){
+    public Carta getCarta_en_mano() {
+        return carta_en_mano;
+    }
+    public void setCarta_en_mano(Carta carta_en_mano) {
         this.carta_en_mano = carta_en_mano;
+    }
+    public Nodo_2(Carta carta_en_mano){
+        Carta carta = carta_en_mano;
+        next = this;
     }
 }
